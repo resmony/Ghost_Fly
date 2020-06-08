12420dfd09a9db7153e70d3255a39ac38aa08382
Résmony <resmony.muniz@dcomp.ufs.br>
2020-06-08 10:12:38 -0300

    Update CameraView.hpp

--- CameraView.hpp
+++ CameraView.hpp
@@ -6,7 +6,6 @@
 #include <glm/glm.hpp>
 #include <glm/gtx/transform.hpp>
 #include <glm/gtc/quaternion.hpp>
-#include <glm/gtx/quaternion.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
